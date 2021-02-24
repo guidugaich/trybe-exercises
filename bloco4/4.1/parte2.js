238775ef8502578ff4e047d860098dc83caefd35
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-02-24 15:47:42 -0300

    initial commit

---
+++ bloco4/4.1/parte2.js
@@ -0,0 +1,11 @@
+const name = 'guilherme';
+const birthCity = 'Sao Paulo';
+let birthYear = 1992 
+console.log(name, birthCity, birthYear);
+
+birthYear = 2020;
+
+console.log(birthYear);
+
+birthCity = 'Rio';
+console.log(birthCity);
