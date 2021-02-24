af782f4d9768ba7bec97eaf3c0ca08527769b4dd
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-02-24 19:20:58 -0300

    Exercicio 9 - verificando se pelo menos um em tres numeros eh impar

--- bloco4/4.1/ex9.js
+++ bloco4/4.1/ex9.js
@@ -2,7 +2,7 @@ let a = 2;
 let b = 4;
 let c = 6;
 
-if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0) {
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
     console.log(true);
 } else {
     console.log(false);
