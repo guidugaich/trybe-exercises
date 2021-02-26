fe792febb472cca1a5713c3679dc85000cfe8404
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-02-26 18:55:54 -0300

    Exercicio 4 - piramide equilatera

---
+++ bloco4/4.3/ex4.js
@@ -0,0 +1,20 @@
+let n = 21;
+
+// definir altura da piramide em linhas
+let pyramidHeight = Math.ceil(n/2);
+
+let asteriscs = 1
+
+for (index = 0; index < pyramidHeight; index += 1) {
+    let string = '';
+    let spacesToTheLeft = (n - asteriscs) / 2; 
+    for (let j = 0; j < (spacesToTheLeft + asteriscs); j += 1) {
+        if (j <  spacesToTheLeft) {
+            string += ' ';
+        } else {
+            string += '*';
+        }
+    }
+    console.log(string);
+    asteriscs += 2;  
+}
