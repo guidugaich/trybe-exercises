6b4fd0f4044bc7acf6aa93b81e0bb0bfbc7d0d34
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-02-26 11:28:50 -0300

    Ex Bonus 3 - multiplicando pelo valor seguinte dentro do array

---
+++ bloco4/4.2/exbonus3.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numbersMultiplied = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    
+    if (index == numbers.length - 1) {
+        numbersMultiplied.push(numbers[index] * 2);
+    } else {
+        numbersMultiplied.push(numbers[index] * numbers[index + 1]);
+    }
+    console.log(numbersMultiplied);
+}
+
