b7885ff88dacce664e9e04c57f2a1b15dc71fa3e
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-02-26 19:35:36 -0300

    Exercicio 5 - piramide vazia, bem dificil!

--- bloco4/4.3/ex5bonus.js
+++ bloco4/4.3/ex5bonus.js
@@ -3,12 +3,12 @@ let n = 35;
 let pyramidHeight = Math.ceil(n/2);
 let pyramidMargin = Math.floor(n/2);
 
-for (index = 0; index < pyramidHeight; index += 1) {
+for (let rowIndex = 0; rowIndex < pyramidHeight; rowIndex += 1) {
     let string = '';
-    for (let j = 0; j < n; j += 1) {
-        if (j == (pyramidMargin) || j == (n - pyramidMargin - 1)) {
+    for (let stringIndex = 0; stringIndex < n; stringIndex += 1) {
+        if (stringIndex == (pyramidMargin) || stringIndex == (n - pyramidMargin - 1)) {
             string += '*';
-        } else if (index == pyramidHeight - 1) {
+        } else if (rowIndex == pyramidHeight - 1) {
             string += '*';
         } else {
             string += ' ';
