f759ab1ca83c5acd69682e1c00dcfdfe8f42bc89
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-01 18:34:03 -0300

    Parte 2 - funcoes inicando com exercicio 1

---
+++ bloco4/4.4/parte2-1.js
@@ -0,0 +1,14 @@
+function isPalindrome(word) {
+    let reverseWord = '';
+    for (let char = 1; char <= word.length; char += 1) {
+        reverseWord += word[word.length - char];
+    }
+    
+    if (word == reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isPalindrome('oio'));
