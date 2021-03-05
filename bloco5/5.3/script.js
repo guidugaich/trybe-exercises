34072c87f5f456a6e156ff0391fad98062ece1e4
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-05 18:58:04 -0300

    Exercicio 1 feito

--- bloco5/5.3/script.js
+++ bloco5/5.3/script.js
@@ -14,3 +14,24 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  // Exercicio 1
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const holidays = [24, 25, 31];
+  const fridays = [4, 11, 18, 25];
+
+  for (let day of dezDaysList) {
+      let dayElement = document.createElement('li');
+      dayElement.className = 'day';
+      dayElement.innerText = day;
+   
+      if (holidays.includes(day)) {
+          dayElement.classList.add('holiday');
+      }
+
+      if (fridays.includes(day)) {
+        dayElement.classList.add('friday');
+    }
+     
+      document.getElementById('days').appendChild(dayElement);
+  }
