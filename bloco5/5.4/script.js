5150d55984cf9bd90acda9d3ebea7ecc7640863c
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-08 23:19:13 -0300

    added local storage data, but didnt use it yet

--- bloco5/5.4/script.js
+++ bloco5/5.4/script.js
@@ -10,25 +10,30 @@ window.onload = function () {
     bgColorBtn.addEventListener('click', function () {
         let bgColor = document.getElementById('bg-color').value;
         text.style.backgroundColor = bgColor;
+        localStorage.setItem('bg-color', bgColor);
     });
 
     textColorBtn.addEventListener('click', function () {
         let textColor = document.getElementById('text-color').value;
         text.style.color = textColor;
+        localStorage.setItem('text-color', textColor);
     });
 
     fontSizeBtn.addEventListener('click', function () {
         let fontSize = document.getElementById('font-size').value;
         text.style.fontSize = fontSize;
+        localStorage.setItem('font-size', fontSize);
     });
 
     lineHeightBtn.addEventListener('click', function () {
         let lineHeight = document.getElementById('line-height').value;
         text.style.lineHeight = lineHeight;
+        localStorage.setItem('line-height', lineHeight);
     });
 
     fontFamilyBtn.addEventListener('click', function () {
         let fontFamily = document.getElementById('font-family').value;
         text.style.fontFamily = fontFamily;
+        localStorage.setItem('font-family', fontFamily);
     });
 }
