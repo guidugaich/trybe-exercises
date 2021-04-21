e9e12d6222e50a1f6d9bccfe7de96af7b3437330
guidugaich <guidugaich.dev@gmail.com>
2021-04-21 18:49:22 -0300

    i have no idea whats going on

--- bloco8/8.5/ex1.js
+++ bloco8/8.5/ex1.js
@@ -7,8 +7,6 @@ const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rec) => console.log(rectangleArea(...rec)))
-
 rectangles.forEach((rectangle) => {
   assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]) // altere a chamada da funcao rectangleArea
 });
