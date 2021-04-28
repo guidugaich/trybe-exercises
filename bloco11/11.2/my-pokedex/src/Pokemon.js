f23b15c66be430c34a3a318eb90e23e053b0f1f4
guidugaich <guidugaich.dev@gmail.com>
2021-04-28 15:33:19 -0300

    created Pokemon component and added css

---
+++ bloco11/11.2/my-pokedex/src/Pokemon.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Pokemon.css'
+
+function Pokemon(props) {
+  return (
+    <div class='pokemon'>
+      <div class='pokeinfo'>
+        <p>{props.pokemon.name}</p>
+        <p>{props.pokemon.type}</p>
+        <p>Weight: {props.pokemon.averageWeight.value}</p>
+      </div>
+      <img src={props.pokemon.image} className='pokeimg'></img>
+    </div>
+  )
+}
+
+export default Pokemon
