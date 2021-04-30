80baaa4762a407e9ea87e18bff4dfccf684bf53d
guidugaich <guidugaich.dev@gmail.com>
2021-04-30 14:40:06 -0300

    exercicios 12.1

--- bloco12/12.1/fancy-buttons/src/App.js
+++ bloco12/12.1/fancy-buttons/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import React from 'react';
 
-const handleClick = () => console.log('Cricou danado')
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      nClicks: 0
+    }
+  }
+
+  handleClick() {
+    console.log('Cricou danado');
+    console.log(this.state);
+    this.setState((prevState, _props) => ({
+      nClicks: prevState.nClicks + 1
+    }));
+  };
+
   render() {
     return (
-      <button onClick={handleClick}>Clica neu</button>
+      <div>
+        <button onClick={this.handleClick} className='fancyButton'>{this.state.nClicks}</button>
+      </div>
     );
   }
 }
