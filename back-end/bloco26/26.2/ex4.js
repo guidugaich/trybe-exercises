6c6840cd41416da98e90fdf5c0ccee2301febecb
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-26 22:05:52 -0300

    exercicios dia 26.2

--- back-end/bloco26/26.2/ex4.js
+++ back-end/bloco26/26.2/ex4.js
@@ -10,13 +10,41 @@ function readCharacterFromFile() {
 
 function getCharacterById(id) {
   return new Promise((resolve, reject) => {
-    fs.readFile(f, 'utf8').then(data => {
-      const characters = JSON.parse(data)
-      resolve(characters.find(c => c.id == id))
-    })
+    fs.readFile(f, 'utf8')
+      .then(data => {
+        const characters = JSON.parse(data)
+        resolve(characters.find(c => c.id == id))
+      })
   })
 }
 
-// readCharacterFromFile();
-getCharacterById(3).then(res => console.log(res));
+async function changeFile1() {
+  const oldContent = await fs.readFile('simpsons.json')
+  const oldContentParsed = JSON.parse(oldContent);
+  const newContent = oldContentParsed.filter(c => {
+    if (c.id === '10' || c.id === '6') return false
+    return true
+  })
+  console.log(newContent)
+  fs.writeFile('simpsons.json', JSON.stringify(newContent))
+}
+
+async function changeFile2() {
+  const oldContent = await fs.readFile('simpsons.json')
+  let oldContentParsed = JSON.parse(oldContent);
+  oldContentParsed = oldContentParsed.map(c => ({ ...c, id: parseInt(c.id)}));
+  console.log(oldContentParsed)
+  const newContent = oldContentParsed.filter(c => {
+    if (c.id >= 1 && c.id <= 4) return true
+    return false
+  })
+  console.log(newContent)
+  fs.writeFile('simpsonFamily.json', JSON.stringify(newContent))
+}
+
+
+//readCharacterFromFile();
+//getCharacterById(34).then(res => console.log(res));
+// changeFile1();
+changeFile2()
 
